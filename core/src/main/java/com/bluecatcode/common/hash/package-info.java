059558b6ecc16e4cc04354db4294e9bd12286d6f
Paweł Prażak <pawel.prazak@bluecatcode.com>
2014-10-04 11:16:39 +0200

    Added Hash utility

---
+++ core/src/main/java/com/bluecatcode/common/hash/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * Hashing utility libraries and interfaces.
+ * <p/>
+ * <h3>Main goals:</h3>
+ * <ul>
+ *     <li>Provide easy to use and less error prone abstraction for hashing operations
+ * </ul>
+ *
+ * <h2>Contents</h2>
+ *
+ * <ul>
+ * <li>{@link com.bluecatcode.common.hash.Hash}
+ * </ul>
+ *
+ * <p/>
+ * @see com.google.common.hash
+ */
+@ParametersAreNonnullByDefault
+package com.bluecatcode.common.hash;
+
+import javax.annotation.ParametersAreNonnullByDefault;
