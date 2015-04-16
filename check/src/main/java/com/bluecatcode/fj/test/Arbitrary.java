e273f946713ae4d89ca15a28d7500cd68d2e86c7
Paweł Prażak <pawel.prazak@bluecatcode.com>
2015-04-16 09:42:47 +0200

    Updated Arbitrary, added tests
    - added 2 continence methods
    - added self-hosted tests for check module

--- check/src/main/java/com/bluecatcode/fj/test/Arbitrary.java
+++ check/src/main/java/com/bluecatcode/fj/test/Arbitrary.java
@@ -6,6 +6,7 @@ import fj.test.Gen;
 import static fj.test.Arbitrary.arbitrary;
 import static fj.test.Gen.choose;
 import static fj.test.Gen.sized;
+import static java.lang.Math.abs;
 
 public class Arbitrary {
 
@@ -13,20 +14,25 @@ public class Arbitrary {
         throw new UnsupportedOperationException();
     }
 
+    public static fj.test.Arbitrary<Integer> arbIntegerNonNegative() {
+        return arbIntegerFrom(0);
+    }
+
+    public static fj.test.Arbitrary<Integer> arbIntegerPositive() {
+        return arbIntegerFrom(1);
+    }
+
     public static fj.test.Arbitrary<Integer> arbIntegerFrom(final Integer start) {
         return arbitrary(sized(new F<Integer, Gen<Integer>>() {
-            public Gen<Integer> f(final Integer i) {
-                return choose(start, i);
+            @Override
+            public Gen<Integer> f(Integer i) {
+                return choose(start, start + abs(i));
             }
         }));
     }
 
     public static fj.test.Arbitrary<Integer> arbIntegerIn(final Integer start, final Integer end) {
-        return arbitrary(sized(new F<Integer, Gen<Integer>>() {
-            public Gen<Integer> f(final Integer i) {
-                return choose(start, end);
-            }
-        }));
+        return arbitrary(choose(start, end));
     }
 
 }
