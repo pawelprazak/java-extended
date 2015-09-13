f784ff6ed43d9b47ff02fb9888affa4d96fb1ae8
Paweł Prażak <pawel.prazak@bluecatcode.com>
2015-09-13 15:22:54 +0200

    Extended Environment with port availability methods

--- guava/src/main/java/com/bluecatcode/common/base/Environment.java
+++ guava/src/main/java/com/bluecatcode/common/base/Environment.java
@@ -3,15 +3,18 @@ package com.bluecatcode.common.base;
 import com.google.common.annotations.Beta;
 import com.google.common.base.Optional;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static com.google.common.base.Strings.emptyToNull;
+import static java.lang.String.format;
 
 /**
- * Provides easy and safe access to environment variables, system properties
+ * Provides easy and safe access to environment variables, system properties, etc.
  */
 @Beta
 public class Environment {
@@ -86,6 +89,65 @@ public class Environment {
         return Optional.fromNullable(
                 emptyToNull(System.getProperty(name))
         );
+    }
+
+    /**
+     * The minimum number of server port number.
+     */
+    public static final int MIN_PORT_NUMBER = 1;
+
+    /**
+     * The maximum number of server port number.
+     */
+    public static final int MAX_PORT_NUMBER = 49151;
+
+    /**
+     * Returns a currently available port number in the specified range.
+     * @param fromPort the lower port limit
+     * @param toPort the upper port limit
+     * @throws IllegalArgumentException if port range is not between {@link #MIN_PORT_NUMBER}
+     *      and {@link #MAX_PORT_NUMBER} or <code>fromPort</code> if greater than <code>toPort</code>.
+     * @return an available port
+     */
+    public static int getNextAvailablePort(int fromPort, int toPort) {
+        if (fromPort < MIN_PORT_NUMBER || toPort > MAX_PORT_NUMBER || fromPort > toPort) {
+            throw new IllegalArgumentException(format("Invalid port range: %d ~ %d", fromPort, toPort));
+        }
+
+        int nextPort = fromPort;
+        //noinspection StatementWithEmptyBody
+        while (!isPortAvailable(nextPort++)) { /* Empty */ }
+        return nextPort;
+    }
+
+    /**
+     * Returns a currently available port number in range from the lower bound to {@link #MAX_PORT_NUMBER}.
+     * @param fromPort the lower port limit
+     * @return an available port
+     */
+    public static int getNextAvailablePort(int fromPort) {
+        return getNextAvailablePort(fromPort, MAX_PORT_NUMBER);
+    }
+
+    /**
+     * Checks if a port is available by opening a socket on it
+     * @param port the port to test
+     * @return true if the port is available
+     */
+    public static boolean isPortAvailable(int port) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Testing port ").append(port).append("\n");
+        try (Socket s = new Socket("localhost", port)) {
+            // If the code makes it this far without an exception it means
+            // something is using the port and has responded.
+            builder.append("Port ").append(port).append(" is not available").append("\n");
+            return false;
+        } catch (IOException e) {
+            builder.append("Port ").append(port).append(" is available").append("\n");
+            return true;
+        } finally {
+            log.info(builder.toString());
+        }
     }
 
     private Environment() {
