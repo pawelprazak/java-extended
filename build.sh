07254b557365be22ab72447d084039ee3a576924
Paweł Prażak <pawel.prazak@bluecatcode.com>
2015-11-27 19:13:08 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 #set -e -u
 
+LINE="---------------------------------------------------------------------------------------------------------------"
+
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
     MVN_CMD="mvn clean install deploy --quiet --settings travis-settings.xml -Pbuild-release -B"
 else
     MVN_CMD="mvn clean test -Pbuild-test -B"
 fi
 
-CODE=$( \
 ${MVN_CMD} -am -pl guava -Dguava.version=15.0 && \
 #${MVN_CMD} -am -pl guava -Dguava.version=16.0 && \
 #${MVN_CMD} -am -pl guava -Dguava.version=17.0 && \
@@ -21,7 +22,6 @@ ${MVN_CMD} -am -pl junit -Djunit.version=4.10 && \
 #${MVN_CMD} -am -pl junit -Djunit.version=4.12 && \
 ${MVN_CMD} -am -pl hamcrest -Dhamcrest.version=1.3 && \
 ${MVN_CMD} -am -pl mockito -Dmockito.version=1.10.19 \
-)
 
 echo "Builds done with code: '${CODE}'"
 
@@ -31,12 +31,19 @@ if [ "${TRAVIS_REPO_SLUG}" == "pawelprazak/java-extended" ] && \
    [ "${TRAVIS_JOB_NUMBER}" == "${TRAVIS_BUILD_NUMBER}.1" ] && \
    [ "${TRAVIS_BRANCH}" == "master" ]; then
 
-  CODE=${CODE} && $(mvn clean test -Pbuild-test jacoco:report coveralls:report)
-  echo "Coveralls report done with code: '${CODE}'"
-  
-  echo "Generating Coverity Report..."
-  CODE=${CODE} && test $(mvn coverity-ondemand:check) != 0
-  echo "Generated Coverity Report."
-fi
+  echo ${LINE}
+  echo "Running tests"
+  echo
+  mvn clean test -Pbuild-test jacoco:report coveralls:report
+  echo
+  echo "Coveralls report done with code"
+  echo ${LINE}
 
-exit ${CODE}
+  echo ${LINE}
+  echo "Generating Coverity Report..."
+  echo
+  test $(mvn coverity-ondemand:check) != 0
+  echo
+  echo "Generated Coverity Report."
+  echo ${LINE}
+fi
