4ac8f94ba0b102002b78e8c95d6b483d7c0ed677
Paweł Prażak <pawel.prazak@bluecatcode.com>
2015-11-27 21:32:01 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ LINE="--------------------------------------------------------------------------
 
 echo "Building branch: '${TRAVIS_BRANCH}', tag: '${TRAVIS_TAG}'"
 
-if [ "${TRAVIS_BRANCH}" == =~ ^release.* ]; then
+if [ "${TRAVIS_BRANCH}" =~ ^release.* ]; then
     MVN_CMD="mvn clean install deploy --quiet --settings travis-settings.xml -Pbuild-release -B"
 else
     MVN_CMD="mvn clean install -Pbuild-test -B"
