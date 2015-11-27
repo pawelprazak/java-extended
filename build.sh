74050c90c6261a7c0f5e744432fa67de317c35ce
Paweł Prażak <pawel.prazak@bluecatcode.com>
2015-11-27 20:10:25 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-#set -e -u
+abort()
+{
+    echo >&2 '
+***************
+*** ABORTED ***
+***************
+'
+    echo "An error occurred. Exiting..." >&2
+    exit 1
+}
+
+trap 'abort' 0
+
+set -e -u
 
 LINE="---------------------------------------------------------------------------------------------------------------"
 
@@ -10,18 +23,18 @@ else
     MVN_CMD="mvn clean test -Pbuild-test -B"
 fi
 
-${MVN_CMD} -am -pl guava -Dguava.version=15.0 && \
-#${MVN_CMD} -am -pl guava -Dguava.version=16.0 && \
-#${MVN_CMD} -am -pl guava -Dguava.version=17.0 && \
-#${MVN_CMD} -am -pl guava -Dguava.version=18.0 && \
-${MVN_CMD} -am -pl time -Djoda-time.version=2.1 && \
-#${MVN_CMD} -am -pl time -Djoda-time.version=2.3 && \
-#${MVN_CMD} -am -pl time -Djoda-time.version=2.9.1 && \
-${MVN_CMD} -am -pl junit -Djunit.version=4.10 && \
-#${MVN_CMD} -am -pl junit -Djunit.version=4.11 && \
-#${MVN_CMD} -am -pl junit -Djunit.version=4.12 && \
-${MVN_CMD} -am -pl hamcrest -Dhamcrest.version=1.3 && \
-${MVN_CMD} -am -pl mockito -Dmockito.version=1.10.19 \
+${MVN_CMD} -am -pl guava -Dguava.version=15.0
+#${MVN_CMD} -am -pl guava -Dguava.version=16.0
+#${MVN_CMD} -am -pl guava -Dguava.version=17.0
+#${MVN_CMD} -am -pl guava -Dguava.version=18.0
+${MVN_CMD} -am -pl time -Djoda-time.version=2.1
+#${MVN_CMD} -am -pl time -Djoda-time.version=2.3
+#${MVN_CMD} -am -pl time -Djoda-time.version=2.9.1
+${MVN_CMD} -am -pl junit -Djunit.version=4.10
+#${MVN_CMD} -am -pl junit -Djunit.version=4.11
+#${MVN_CMD} -am -pl junit -Djunit.version=4.12
+${MVN_CMD} -am -pl hamcrest -Dhamcrest.version=1.3
+${MVN_CMD} -am -pl mockito -Dmockito.version=1.10.19
 
 if [ "${TRAVIS_REPO_SLUG}" == "pawelprazak/java-extended" ] && \
    [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" ] && \
@@ -45,3 +58,11 @@ if [ "${TRAVIS_REPO_SLUG}" == "pawelprazak/java-extended" ] && \
   echo "Generated Coverity Report."
   echo ${LINE}
 fi
+
+trap : 0
+
+echo >&2 '
+************
+*** DONE ***
+************
+'
