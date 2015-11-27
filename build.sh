2871240fec81e3e5b30055b903b8ecd7ad6446fd
Paweł Prażak <pawel.prazak@bluecatcode.com>
2015-11-27 19:03:40 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ else
     MVN_CMD="mvn clean test -Pbuild-test -B"
 fi
 
-${MVN_CMD} -am -pl guava -Dguava.version=15.0 && \
+CODE=${MVN_CMD} -am -pl guava -Dguava.version=15.0 && \
 #${MVN_CMD} -am -pl guava -Dguava.version=16.0 && \
 #${MVN_CMD} -am -pl guava -Dguava.version=17.0 && \
 #${MVN_CMD} -am -pl guava -Dguava.version=18.0 && \
@@ -21,15 +21,20 @@ ${MVN_CMD} -am -pl junit -Djunit.version=4.10 && \
 ${MVN_CMD} -am -pl hamcrest -Dhamcrest.version=1.3 && \
 ${MVN_CMD} -am -pl mockito -Dmockito.version=1.10.19
 
+echo "Builds done with code: '${CODE}'"
+
 if [ "${TRAVIS_REPO_SLUG}" == "pawelprazak/java-extended" ] && \
    [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" ] && \
    [ "${TRAVIS_PULL_REQUEST}" == "false" ] && \
    [ "${TRAVIS_JOB_NUMBER}" == "${TRAVIS_BUILD_NUMBER}.1" ] && \
    [ "${TRAVIS_BRANCH}" == "master" ]; then
+
+  CODE=${CODE} && mvn clean test -Pbuild-test jacoco:report coveralls:report
+  echo "Coveralls report done with code: '${CODE}'"
+  
   echo "Generating Coverity Report..."
-
-  mvn clean test -Pbuild-test jacoco:report coveralls:report && \
-    test $(mvn coverity-ondemand:check) != 0
-
+  CODE=${CODE} && test $(mvn coverity-ondemand:check) != 0
   echo "Generated Coverity Report."
 fi
+
+exit ${CODE}
