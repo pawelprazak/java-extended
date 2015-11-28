ddcc60750c77fa4affe71d900913e33206cc8d6a
Paweł Prażak <pawel.prazak@bluecatcode.com>
2015-11-28 15:16:27 +0100

    Updated .travis.yml and build.sh

--- build.sh
+++ build.sh
@@ -38,6 +38,19 @@ ${MVN_CMD} -am -pl junit -Djunit.version=4.10
 ${MVN_CMD} -am -pl hamcrest -Dhamcrest.version=1.3
 ${MVN_CMD} -am -pl mockito -Dmockito.version=1.10.19
 
+if [ "${TRAVIS_REPO_SLUG}" == "pawelprazak/java-extended" ] && \
+   [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" ] && \
+   [ "${TRAVIS_PULL_REQUEST}" == "false" ] && \
+   [ "${TRAVIS_JOB_NUMBER}" == "${TRAVIS_BUILD_NUMBER}.1" ] && \
+   [ "${TRAVIS_BRANCH}" == "master" ]; then
+
+  echo ${LINE}
+  echo
+  mvn clean test -Pbuild-test,enable-mutation-tests -B
+  echo
+  echo ${LINE}
+fi
+
 trap : 0
 
 echo >&2 '
