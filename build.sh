ab1ac00553dded6e8cd200f77f4fb4160d20f65c
Paweł Prażak <pawel.prazak@bluecatcode.com>
2015-11-28 22:44:11 +0100

    Updated build scripts

--- build.sh
+++ build.sh
@@ -20,9 +20,9 @@ LINE="--------------------------------------------------------------------------
 echo "Building branch: '${TRAVIS_BRANCH}', tag: '${TRAVIS_TAG}'"
 
 if [[ "${TRAVIS_BRANCH}" =~ ^release.* ]]; then
-    MVN_CMD="mvn test --settings travis-settings.xml -Pbuild-release -B"
+    MVN_CMD="mvn deploy jacoco:report --settings travis-settings.xml -Pbuild-release  -B"
 else
-    MVN_CMD="mvn test -Pbuild-test -B"
+    MVN_CMD="mvn test jacoco:report -Pbuild-test -B"
 fi
 
 ${MVN_CMD} -am -pl guava -Dguava.version=15.0
@@ -37,19 +37,6 @@ ${MVN_CMD} -am -pl junit -Djunit.version=4.11
 ${MVN_CMD} -am -pl junit -Djunit.version=4.12
 ${MVN_CMD} -am -pl hamcrest -Dhamcrest.version=1.3
 ${MVN_CMD} -am -pl mockito -Dmockito.version=1.10.19
-
-if [ "${TRAVIS_REPO_SLUG}" == "pawelprazak/java-extended" ] && \
-   [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" ] && \
-   [ "${TRAVIS_PULL_REQUEST}" == "false" ] && \
-   [ "${TRAVIS_JOB_NUMBER}" == "${TRAVIS_BUILD_NUMBER}.1" ] && \
-   [ "${TRAVIS_BRANCH}" == "master" ]; then
-
-  echo ${LINE}
-  echo
-  mvn clean test jacoco:report -Pbuild-test,enable-mutation-tests -B
-  echo
-  echo ${LINE}
-fi
 
 trap : 0
 
