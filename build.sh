46dacfc2f629ab4dade6ea9bacdff5bfc085b376
Paweł Prażak <pawel.prazak@bluecatcode.com>
2015-11-29 10:44:46 +0100

    Updated build scripts

--- build.sh
+++ build.sh
@@ -18,23 +18,28 @@ set -e -u
 echo "Building branch: '${TRAVIS_BRANCH}', tag: '${TRAVIS_TAG}'"
 
 if [[ "${TRAVIS_BRANCH}" =~ ^release.* ]]; then
-    MVN_CMD="mvn deploy jacoco:report --settings travis-settings.xml -Pbuild-release  -B"
+    MVN_CMD="mvn deploy --settings travis-settings.xml -Pbuild-release -B"
 else
     MVN_CMD="mvn test jacoco:report -Pbuild-test -B"
 fi
 
-${MVN_CMD}
-${MVN_CMD} -am -pl guava -Dguava.version=15.0
-${MVN_CMD} -am -pl guava -Dguava.version=16.0
+${MVN_CMD} jacoco:report -Pbuild-release,enable-unit-tests,enable-integration-tests
+
+#${MVN_CMD} -am -pl guava -Dguava.version=18.0
 ${MVN_CMD} -am -pl guava -Dguava.version=17.0
-${MVN_CMD} -am -pl guava -Dguava.version=18.0
-#${MVN_CMD} -am -pl time -Djoda-time.version=2.1
-#${MVN_CMD} -am -pl time -Djoda-time.version=2.3
+${MVN_CMD} -am -pl guava -Dguava.version=16.0.1
+${MVN_CMD} -am -pl guava -Dguava.version=15.0
+
 #${MVN_CMD} -am -pl time -Djoda-time.version=2.9.1
-#${MVN_CMD} -am -pl junit -Djunit.version=4.10
+#${MVN_CMD} -am -pl time -Djoda-time.version=2.3
+${MVN_CMD} -am -pl time -Djoda-time.version=2.1
+
+#${MVN_CMD} -am -pl junit -Djunit.version=4.12
 ${MVN_CMD} -am -pl junit -Djunit.version=4.11
-${MVN_CMD} -am -pl junit -Djunit.version=4.12
+${MVN_CMD} -am -pl junit -Djunit.version=4.10
+
 #${MVN_CMD} -am -pl hamcrest -Dhamcrest.version=1.3
+
 #${MVN_CMD} -am -pl mockito -Dmockito.version=1.10.19
 
 trap : 0
