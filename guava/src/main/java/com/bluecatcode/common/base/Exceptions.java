319fb29774cbe6493d4808d189e7b0ceeb40b5b2
Pawel Prażak <pawelprazak@gmail.com>
2016-02-05 15:08:41 +0100

    Update Exceptions with reflection based throwable factory

--- guava/src/main/java/com/bluecatcode/common/base/Exceptions.java
+++ guava/src/main/java/com/bluecatcode/common/base/Exceptions.java
@@ -3,6 +3,11 @@ package com.bluecatcode.common.base;
 import com.bluecatcode.core.exceptions.UncheckedException;
 import com.google.common.base.Function;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class Exceptions {
 
     private Exceptions() {
@@ -11,6 +16,54 @@ public class Exceptions {
 
     public static Function<Exception, RuntimeException> uncheckedException() {
         return e -> e instanceof RuntimeException ? (RuntimeException) e : new UncheckedException(e);
+    }
+
+    public static <E extends Exception> E exception(Class<E> throwableType) {
+        return throwable(throwableType);
+    }
+
+    public static <E extends Throwable> E throwable(Class<E> throwableType) {
+        return throwable(throwableType, parameters(), arguments());
+    }
+
+    public static <E extends Exception> E exception(Class<E> throwableType,
+                                                    CheckedFunction<Class<E>, Constructor<E>> constructorSupplier,
+                                                    CheckedFunction<Constructor<E>, E> instanceSupplier) {
+        return throwable(throwableType, constructorSupplier, instanceSupplier);
+    }
+
+    public static <E extends Throwable> E throwable(Class<E> throwableType,
+                                                    CheckedFunction<Class<E>, Constructor<E>> constructorSupplier,
+                                                    CheckedFunction<Constructor<E>, E> instanceSupplier) {
+        checkArgument(!Modifier.isAbstract(throwableType.getModifiers()),
+                "Expected non-abstract throwable type, got: '%s'", throwableType.getCanonicalName());
+        final Constructor<E> constructor;
+        try {
+            constructor = constructorSupplier.apply(throwableType);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Expected a throwable with (String) constructor", e);
+        }
+        try {
+            return instanceSupplier.apply(constructor);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Expected an instantiable throwable.", e);
+        }
+    }
+
+    public static <E> CheckedFunction<Class<E>, Constructor<E>> parameters(Class<?> parameterType) {
+        return type -> type.getConstructor(parameterType);
+    }
+
+    public static <E> CheckedFunction<Class<E>, Constructor<E>> parameters(Class<?>... parameterTypes) {
+        return type -> type.getConstructor(parameterTypes);
+    }
+
+    public static <E> CheckedFunction<Constructor<E>, E> arguments(Object arg) {
+        return constructor -> constructor.newInstance(arg);
+    }
+
+    public static <E> CheckedFunction<Constructor<E>, E> arguments(Object... args) {
+        return constructor -> constructor.newInstance(args);
     }
 
 }
---
+++ guava/src/test/groovy/com/bluecatcode/common/base/ExceptionsTest.groovy
@@ -0,0 +1,59 @@
+package com.bluecatcode.common.base
+
+import spock.lang.FailsWith
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.bluecatcode.common.base.Exceptions.*
+
+class ExceptionsTest extends Specification {
+
+    @Unroll("#type.simpleName '#message' '#cause'")
+    def "should create throwable"() {
+        given:
+        def exception0 = exception(type)
+        def exception1 = exception(type, parameters(String.class), arguments(message))
+        def exception2 = exception(type, parameters(String.class, Throwable.class), arguments(message, cause))
+
+        expect:
+        exception0 != null
+        exception1 != null
+        exception2 != null
+        exception0.class == type
+        exception1.class == type
+        exception2.class == type
+        exception1.message == message
+        exception2.class == type
+        exception2.message == message
+
+        where:
+        type                     | message        | cause
+        IllegalArgumentException | null           | null
+        IllegalArgumentException | "test message" | null
+        IllegalArgumentException | "test message" | new IllegalArgumentException()
+    }
+
+    @FailsWith(IllegalArgumentException)
+    @Unroll("#type.simpleName '#parameters' '#args' -> throws IAE")
+    def "should throw on illegal arguments"() {
+        expect:
+        exception(type, parameters, args)
+
+        where:
+        type              | parameters                          | args
+        TestException     | null                                | null
+        TestException     | parameters(Long.class, Float.class) | null
+        TestException     | null                                | arguments(1L, 1.0f)
+        AbstractException | null                                | null
+    }
+
+    class TestException extends Exception {
+        private TestException(String var1) {
+            super(var1)
+        }
+    }
+
+    abstract class AbstractException extends Exception {
+        /* Empty */
+    }
+}
